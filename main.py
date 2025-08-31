e088ca67db99889291f633d2638eda670b92d679
alik-r <alikamran.rza@gmail.com>
2025-09-01 00:56:58 +0400

    Upgrade edupage_api to 0.12.1

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
-import re
 
-from edupage_api import Edupage
-from edupage_api.exceptions import BadCredentialsException
-from datetime import datetime, timedelta
+from edupage_api import *
+from edupage_api.exceptions import *
+
+from datetime import datetime
 
 from dotenv import load_dotenv
 
@@ -17,42 +17,14 @@ edupage = Edupage()
 
 try:
     edupage.login(EDUPAGE_USERNAME, EDUPAGE_PASSWORD, EDUPAGE_SCHOOL_SUBDOMAIN)
+    now = datetime.now()
+    next_ringing_time = edupage.get_next_ringing_time(now)
+    print("Next ringing time is " + str(next_ringing_time.time))
 except BadCredentialsException:
     print("Wrong username or password!")
+except CaptchaException:
+    print("Captcha required")
+except Exception as e:
+    print(f"error: {str(e)}")
+finally:
     exit()
-    
-
-class MyRingingTime:
-    def __init__(self):
-        self.ringing_time_str = str(self.get_next_ringing_time_now())
-        self.datetime = self.parse_datetime(self.ringing_time_str)
-    
-    def get_next_ringing_time_now(self):
-        now = datetime.now()
-        return edupage.get_next_ringing_time(now)
-
-    def parse_datetime(self, ringing_time_str):
-        match = re.search(r"datetime\.datetime\(([\d, ]+)\)", ringing_time_str)
-        
-        if match:
-            datetime_values = match.group(1).split(", ")
-            date = datetime(
-                year=int(datetime_values[0]),
-                month=int(datetime_values[1]),
-                day=int(datetime_values[2]),
-                hour=int(datetime_values[3]),
-                minute=int(datetime_values[4])
-            )
-            return date
-        else:
-            return None
-
-    def __str__(self):
-        return self.datetime.strftime("%A, %d %B %Y at %I:%M %p")
-
-# ringing_time = MyRingingTime()
-# print(ringing_time)
-
-date_after_tomorrow = datetime.now() + timedelta(days=2)
-
-print(edupage.get_my_timetable(date_after_tomorrow))
